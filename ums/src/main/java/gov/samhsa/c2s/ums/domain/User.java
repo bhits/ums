307f555d059937cba8a0153789451afdd4bcd93b
Shruti Rao <shruti.rao@feisystems.com>
2017-04-10 17:23:18 -0400

    Rename to uaa_users_id
    #13211:As a developer, I will have implemented the Staff Admin Backend for registering a patient

--- ums/src/main/java/gov/samhsa/c2s/ums/domain/User.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/domain/User.java
@@ -77,7 +77,7 @@ public class User {
     @Pattern(regexp = "(\\d{3}-?\\d{2}-?\\d{4})*")
     private String socialSecurityNumber;
 
-    private String uaa_user_id;
+    private String uaa_users_id;
 
     /**
      * The telephone.
