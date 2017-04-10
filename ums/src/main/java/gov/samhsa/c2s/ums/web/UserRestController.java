d34687d36583c9fb42a6e6564099769c6471607b
Shruti Rao <shruti.rao@feisystems.com>
2017-04-10 14:53:39 -0400

    Add signature for update and delete user in the controller
    #13211:As a developer, I will have implemented the Staff Admin Backend for registering a patient

--- ums/src/main/java/gov/samhsa/c2s/ums/web/UserRestController.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/web/UserRestController.java
@@ -4,7 +4,9 @@ import gov.samhsa.c2s.ums.service.UserService;
 import gov.samhsa.c2s.ums.service.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,10 +20,36 @@ public class UserRestController {
     @Autowired
     UserService userService;
 
+    /**
+     * Crate User
+     * @param userDto
+     */
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveUser(@Valid @RequestBody UserDto userDto) {
         userService.saveUser(userDto);
+    }
+
+
+    /**
+     * Delete User
+     * @param userId
+     */
+    @PutMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser(@PathVariable Long userId) {
+        //userService.deleteUser(userId);
+    }
+
+    /**
+     * Update User
+     * @param userId
+     * @param userDto
+     */
+    @PutMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateUser(@PathVariable Long userId, @Valid @RequestBody UserDto userDto) {
+        //userService.updateUser(userId, userDto);
     }
 
 }
