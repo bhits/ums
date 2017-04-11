b58c6d07f328533cfa4afe42025d1e26894ddb28
Shruti Rao <shruti.rao@feisystems.com>
2017-04-11 12:15:04 -0400

    Update method name
    #13211:As a developer, I will have implemented the Staff Admin Backend for registering a patient

--- ums/src/main/java/gov/samhsa/c2s/ums/domain/UserRepository.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/domain/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Patient, Long> {
-    Optional<User> findOnebyId(Long userId);
+    Optional<User> findOneByIdAndIsDeleted(Long userId, boolean isDeleted);
 }
