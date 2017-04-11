ceb76fc50bc4abe18135d5a2b4c164d964c59f66
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-11 14:29:15 -0400

    Merge branch 'master' of https://github.com/FEISystems/ums
    Conflicts:
    	ums/src/main/java/gov/samhsa/c2s/ums/service/UserServiceImpl.java
    	ums/src/main/java/gov/samhsa/c2s/ums/web/UserRestController.java

--- ums/src/main/java/gov/samhsa/c2s/ums/service/UserServiceImpl.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/service/UserServiceImpl.java
@@ -3,20 +3,29 @@ package gov.samhsa.c2s.ums.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.samhsa.c2s.ums.domain.User;
-import gov.samhsa.c2s.ums.domain.User;
 import gov.samhsa.c2s.ums.domain.UserRepository;
 import gov.samhsa.c2s.ums.service.dto.UserDto;
+import gov.samhsa.c2s.ums.service.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import gov.samhsa.c2s.ums.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService {
-    UserRepository  userRepository;
+    private final UserRepository  userRepository;
+
+    private final ModelMapper modelMapper;
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository, ObjectMapper objectMapper) {
+        this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public void saveUser(UserDto userDto) {
