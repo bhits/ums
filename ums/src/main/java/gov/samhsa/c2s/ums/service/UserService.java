c908a77e617dc9a4c46312ad94fd529e69329321
Shruti Rao <shruti.rao@feisystems.com>
2017-04-11 15:44:37 -0400

    Add new APIs  for searching an User
    #13211:As a developer, I will have implemented the Staff Admin Backend for registering a patient

--- ums/src/main/java/gov/samhsa/c2s/ums/service/UserService.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/service/UserService.java
@@ -2,6 +2,11 @@ package gov.samhsa.c2s.ums.service;
 
 import gov.samhsa.c2s.ums.service.dto.UserDto;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public interface UserService {
     @Transactional
@@ -15,5 +20,16 @@ public interface UserService {
 
     @Transactional(readOnly = true)
     Object getUser(Long userId);
+
+    @Transactional(readOnly = true)
+    List<UserDto> getAllUsers();
+
+    @Transactional(readOnly = true)
+    List<UserDto> searchUsersByFirstNameAndLastName(StringTokenizer token);
+
+    @Transactional(readOnly = true)
+    List<UserDto> searchUsersByDemographic(String firstName, String lastName, Date birthDate, String genderCode);
+
+
 
 }
--- ums/src/main/java/gov/samhsa/c2s/ums/web/UserRestController.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/web/UserRestController.java
@@ -3,6 +3,7 @@ package gov.samhsa.c2s.ums.web;
 import gov.samhsa.c2s.ums.service.UserService;
 import gov.samhsa.c2s.ums.service.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
 
 @RestController
 @RequestMapping("/users")
@@ -41,6 +46,11 @@ public class UserRestController {
 
     /**
      * Update User
+     * CAUTION: This method is only for use by admin users, and the SecurityConfig
+     * currently enforces that requirement with appropriate OAuth scope(s). If an
+     * updatePatient function is required in the future for patient users to update
+     * their own accounts, then a separate controller method should be created which
+     * will enforce patient users only being able to update their own patient records.
      * @param userId
      * @param userDto
      */
@@ -59,4 +69,50 @@ public class UserRestController {
     public Object getUser(@PathVariable Long userId){
         return userService.getUser(userId);
     }
+
+    /* CAUTION: This method is only for use by admin users, and the SecurityConfig
+    currently enforces that requirement with appropriate OAuth scope(s). Non-admin
+    users should never be allowed to search for a patient record(s). */
+    /**
+     * Find All Users
+     * @return
+     */
+    @GetMapping("/search")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    /* CAUTION: This method is only for use by admin users, and the SecurityConfig
+    currently enforces that requirement with appropriate OAuth scope(s). Non-admin
+    users should never be allowed to search for a patient record(s). */
+    /**
+     * Find All Users that match the First Name and the Last.
+     * @param token
+     * @return
+     */
+    @GetMapping(value = "/search/{token}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> searchUsersByFirstNameAndLastName(@PathVariable String token) {
+        StringTokenizer tokenizer = new StringTokenizer(token, " ");
+        return userService.searchUsersByFirstNameAndLastName(tokenizer);
+    }
+
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     * @param birthDate
+     * @param genderCode
+     * @return
+     */
+    @GetMapping(value = "/search/patientDemographic")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> searchUsersByDemographic(@RequestParam("firstName") String firstName,
+                                                   @RequestParam("lastName") String lastName,
+                                                   @RequestParam("birthDate") @DateTimeFormat(pattern = "MM/dd/yyyy") Date birthDate,
+                                                   @RequestParam("genderCode") String genderCode) {
+        return userService.searchUsersByDemographic(firstName, lastName, birthDate, genderCode);
+    }
+
 }
