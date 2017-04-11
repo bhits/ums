154a721ce2d08bf2b00b52f93a3a5f6a09a812ed
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-11 14:07:09 -0400

    Invoke mapper to save user functionality

--- ums/src/main/java/gov/samhsa/c2s/ums/service/UserServiceImpl.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/service/UserServiceImpl.java
@@ -1,15 +1,41 @@
 package gov.samhsa.c2s.ums.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.samhsa.c2s.ums.domain.User;
 import gov.samhsa.c2s.ums.domain.UserRepository;
 import gov.samhsa.c2s.ums.service.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
-    UserRepository  userRepository;
+
+
+    private final UserRepository  userRepository;
+
+    private final ModelMapper modelMapper;
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository, ObjectMapper objectMapper) {
+        this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
+    }
 
     @Override
-    public void saveUser(UserDto consentDto) {
+    public void saveUser(UserDto userDto) {
+        User user = modelMapper.map(userDto,User.class);
+        try {
+            log.debug(objectMapper.writeValueAsString(user));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- ums/src/main/java/gov/samhsa/c2s/ums/web/UserRestController.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/web/UserRestController.java
@@ -3,11 +3,9 @@ package gov.samhsa.c2s.ums.web;
 import gov.samhsa.c2s.ums.service.UserService;
 import gov.samhsa.c2s.ums.service.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -18,8 +16,8 @@ public class UserRestController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/users")
-    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/")
+   // @ResponseStatus(HttpStatus.CREATED)
     public void saveUser(@Valid @RequestBody UserDto userDto) {
         userService.saveUser(userDto);
     }
