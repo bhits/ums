df8078aac09b1c83ac3df5f7cd2fac6498820eda
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-13 13:40:38 -0400

    Add Sample data for country code
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

---
+++ ums-db-sample/insert_country code lookup data.sql
@@ -0,0 +1,72 @@
+-- ------------------------------------------------------
+-- UMS Sample Data
+-- ------------------------------------------------------
+
+USE `ums`;
+
+--
+-- Insert data for table `country_code`
+--
+INSERT INTO country_code 
+            (id, 
+             code, 
+             code_system, 
+             code_system_name, 
+             code_systemoid, 
+             description, 
+             display_name) 
+VALUES      ('1', 
+             'US', 
+             '1.0.3166.1', 
+             'ISO 3166-1 Country Codes', 
+             'urn:oid:1.0.3166.1', 
+             'United States', 
+             'United States'); 
+
+INSERT INTO country_code 
+            (id, 
+             code, 
+             code_system, 
+             code_system_name, 
+             code_systemoid, 
+             description, 
+             display_name) 
+VALUES      ('2', 
+             'KZ', 
+             '1.0.3166.1', 
+             'ISO 3166-1 Country Codes', 
+             'urn:oid:1.0.3166.1', 
+             'Kazakhstan', 
+             'Kazakhstan'); 
+
+INSERT INTO country_code 
+            (id, 
+             code, 
+             code_system, 
+             code_system_name, 
+             code_systemoid, 
+             description, 
+             display_name) 
+VALUES      ('3', 
+             'IL', 
+             '1.0.3166.1', 
+             'ISO 3166-1 Country Codes', 
+             'urn:oid:1.0.3166.1', 
+             'Israel', 
+             'Israel'); 
+
+INSERT INTO country_code 
+            (id, 
+             code, 
+             code_system, 
+             code_system_name, 
+             code_systemoid, 
+             description, 
+             display_name) 
+VALUES      ('4', 
+             'AW', 
+             '1.0.3166.1', 
+             'ISO 3166-1 Country Codes', 
+             'urn:oid:1.0.3166.1', 
+             'Aruba', 
+             'Aruba'); 
