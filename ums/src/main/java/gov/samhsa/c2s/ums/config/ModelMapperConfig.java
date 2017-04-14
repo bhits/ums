44f60add3588f2c9d0b03f0e1c9e343c12e21aec
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-14 09:29:34 -0400

    Add PropertyType class for AdressDto to Address mapping
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

--- ums/src/main/java/gov/samhsa/c2s/ums/config/ModelMapperConfig.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/config/ModelMapperConfig.java
@@ -1,11 +1,15 @@
 package gov.samhsa.c2s.ums.config;
 
 
+import gov.samhsa.c2s.ums.domain.Address;
 import gov.samhsa.c2s.ums.domain.User;
-import gov.samhsa.c2s.ums.service.dto.TelecomDto;
+import gov.samhsa.c2s.ums.domain.reference.*;
+import gov.samhsa.c2s.ums.service.dto.AddressDto;
 import gov.samhsa.c2s.ums.service.dto.UserDto;
+import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -52,21 +56,57 @@ public class ModelMapperConfig {
         }
     }
 
-/*
-    */
-/**
-     * Converts {@link TelecomDto to @link Telecom }
-     *//*
-
     @Component
-    static class TelecomDtoToTelecom extends PropertyMap<TelecomDto,Telecom> {
+    public static class AdressDtoToAddress extends  PropertyMap<AddressDto, Address>{
+
+        private final StateCodeConverter stateCodeConverter;
+        private final CountryCodeConverter countryCodeConverter;
+        @Autowired
+        AdressDtoToAddress(StateCodeConverter stateCodeConverter, CountryCodeConverter countryCodeConverter) {
+            this.stateCodeConverter = stateCodeConverter;
+            this.countryCodeConverter = countryCodeConverter;
+        }
+
         @Override
         protected void configure() {
-            skip().setUser(null);
+             using(stateCodeConverter).map(source).setStateCode(null);
+             using(countryCodeConverter).map(source).setCountryCode(null);
+         }
+    }
+
+
+    /**
+     * Converts {@link  AddressDto } to {@link StateCode}}
+     */
+    @Component
+    public static class StateCodeConverter extends AbstractConverter<AddressDto, StateCode> {
+
+        private final StateCodeRepository stateCodeRepository;
+        @Autowired
+        public StateCodeConverter(StateCodeRepository stateCodeRepository) {
+            this.stateCodeRepository = stateCodeRepository;
+        }
+        @Override
+        protected StateCode convert(AddressDto source) {
+            return stateCodeRepository.findByCode(source.getStateCode());
         }
     }
-*/
 
 
+    /**
+     * Converts {@link  AddressDto } to {@link CountryCode}}
+     */
+    @Component
+     static class CountryCodeConverter extends AbstractConverter<AddressDto, CountryCode> {
 
+        private final CountryCodeRepository countryCodeRepository;
+        @Autowired
+        public CountryCodeConverter(CountryCodeRepository CountryCodeRepository) {
+            this.countryCodeRepository = CountryCodeRepository;
+        }
+        @Override
+        protected CountryCode convert(AddressDto source) {
+            return countryCodeRepository.findByCode(source.getCountryCode());
+        }
+    }
 }
