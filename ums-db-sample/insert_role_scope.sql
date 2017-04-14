a34569ca006cd149bc54752c81cb888f2198963a
CindyLiFei <cindy.li@feisystems.com>
2017-04-14 01:39:29 -0400

    Add scope to patient using SQL
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

--- ums-db-sample/insert_role_scope.sql
+++ ums-db-sample/insert_role_scope.sql
@@ -13,3 +13,17 @@ INSERT INTO role (`id`, `role_name`) VALUES ('3', 'Guardian');
 INSERT INTO role (`id`, `role_name`) VALUES ('4', 'Provider');
 INSERT INTO role (`id`, `role_name`) VALUES ('5', 'SystemSupport');
 INSERT INTO role (`id`, `role_name`) VALUES ('6', 'StaffUser');
+
+--
+-- Insert data for table `scope`
+--
+
+INSERT INTO scope (`id`, `scope_name`,`scope_description`) VALUES ('1', 'c2sUiApi.read','Read access to C2S Backend API');
+INSERT INTO scope (`id`, `scope_name`,`scope_description`) VALUES ('2', 'c2sUiApi.write','Write access to C2S Backend API');
+
+--
+-- Insert data for table `scope_roles`
+--
+
+INSERT INTO scope_roles (`scopes_id`, `roles_id`) VALUES ('1', '1');
+INSERT INTO scope_roles (`scopes_id`, `roles_id`) VALUES ('2', '1');
