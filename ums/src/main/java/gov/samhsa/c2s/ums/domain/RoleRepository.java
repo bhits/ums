62b91fd87b9f726f24dc16db987250797094d600
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-14 09:30:10 -0400

    Add Repositories for all Entities
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

---
+++ ums/src/main/java/gov/samhsa/c2s/ums/domain/RoleRepository.java
@@ -0,0 +1,9 @@
+package gov.samhsa.c2s.ums.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by sadhana.chandra on 4/14/2017.
+ */
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
