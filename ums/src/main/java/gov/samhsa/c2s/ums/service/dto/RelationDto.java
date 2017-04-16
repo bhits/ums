de413a8abf955576c0aff7e8b2f45cb627df6bc3
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-16 04:16:55 -0400

    Placeholder to capture User Patient Relationship Information
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

---
+++ ums/src/main/java/gov/samhsa/c2s/ums/service/dto/RelationDto.java
@@ -0,0 +1,16 @@
+package gov.samhsa.c2s.ums.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RelationDto {
+    private long userId;
+    private long patientId;
+    private String relationshipCode;
+}
