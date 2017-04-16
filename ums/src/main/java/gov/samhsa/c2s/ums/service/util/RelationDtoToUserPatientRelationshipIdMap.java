1bfcb2dabddcd97b35b24ee9aab44e17f44d01bd
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-16 04:26:40 -0400

    ModelMapper PropertyMap to Convert RelationDto to UserPatientRelationshipId Object
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

---
+++ ums/src/main/java/gov/samhsa/c2s/ums/service/util/RelationDtoToUserPatientRelationshipIdMap.java
@@ -0,0 +1,98 @@
+package gov.samhsa.c2s.ums.service.util;
+
+import gov.samhsa.c2s.ums.domain.Patient;
+import gov.samhsa.c2s.ums.domain.PatientRepository;
+import gov.samhsa.c2s.ums.domain.Relationship;
+import gov.samhsa.c2s.ums.domain.RelationshipRepository;
+import gov.samhsa.c2s.ums.domain.User;
+import gov.samhsa.c2s.ums.domain.UserPatientRelationship;
+import gov.samhsa.c2s.ums.domain.UserRepository;
+import gov.samhsa.c2s.ums.domain.valueobject.UserPatientRelationshipId;
+import gov.samhsa.c2s.ums.service.dto.RelationDto;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Converts {@link  RelationDto } to {@link UserPatientRelationship}}
+ */
+@Component
+public class RelationDtoToUserPatientRelationshipIdMap extends PropertyMap<RelationDto, UserPatientRelationshipId> {
+
+    private final UserConverter userConverter;
+    private final PatientConverter patientConverter;
+    private final RelationshipConverter relationshipConverter;
+
+    public RelationDtoToUserPatientRelationshipIdMap(UserConverter userConverter, PatientConverter patientConverter, RelationshipConverter relationshipConverter) {
+        this.userConverter = userConverter;
+        this.patientConverter = patientConverter;
+        this.relationshipConverter = relationshipConverter;
+    }
+
+    @Override
+    protected void configure() {
+        using(userConverter).map(source).setUser(null);
+        using(patientConverter).map(source).setPatient(null);
+        using(relationshipConverter).map(source).setRelationship(null);
+    }
+
+
+    /**
+     * Converts {@link  RelationDto } to {@link User}}
+     */
+    @Component
+    private static class UserConverter extends AbstractConverter<RelationDto, User> {
+
+        private final UserRepository userRepository;
+
+        @Autowired
+        public UserConverter(UserRepository userRepository) {
+            this.userRepository = userRepository;
+        }
+
+        @Override
+        protected User convert(RelationDto source) {
+            return userRepository.findOne(source.getUserId());
+        }
+    }
+
+    /**
+     * Converts {@link  RelationDto } to {@link Patient}}
+     */
+    @Component
+    private static class PatientConverter extends AbstractConverter<RelationDto, Patient> {
+
+        private final PatientRepository patientRepository;
+
+        @Autowired
+        public PatientConverter(PatientRepository patientRepository) {
+            this.patientRepository = patientRepository;
+        }
+
+        @Override
+        protected Patient convert(RelationDto source) {
+            return patientRepository.findOne(source.getPatientId());
+        }
+    }
+
+
+    /**
+     * Converts {@link  RelationDto } to {@link Relationship}}
+     */
+    @Component
+    private static class RelationshipConverter extends AbstractConverter<RelationDto, Relationship> {
+
+        private final RelationshipRepository relationshipRepository;
+
+        @Autowired
+        public RelationshipConverter(RelationshipRepository relationshipRepository) {
+            this.relationshipRepository = relationshipRepository;
+        }
+
+        @Override
+        protected Relationship convert(RelationDto source) {
+            return relationshipRepository.findByCode(source.getRelationshipCode());
+        }
+    }
+}
