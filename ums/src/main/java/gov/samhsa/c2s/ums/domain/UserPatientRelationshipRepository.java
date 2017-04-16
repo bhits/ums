1f2015a02437e00e7de059d39edd91e98c7b7326
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-16 04:31:18 -0400

    Map UserPatientRelationship to the repository
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

--- ums/src/main/java/gov/samhsa/c2s/ums/domain/UserPatientRelationshipRepository.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/domain/UserPatientRelationshipRepository.java
@@ -3,5 +3,5 @@ package gov.samhsa.c2s.ums.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserPatientRelationshipRepository extends JpaRepository<Relationship, Long> {
+public interface UserPatientRelationshipRepository extends JpaRepository<UserPatientRelationship, Long> {
 }
