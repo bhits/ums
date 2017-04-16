f94fdc8e112d4d324a41c4373e8c184e9bae31ff
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-04-16 04:27:06 -0400

    ModelMapper PropertyMap to Convert UserDto to User Object
    #13211- As a developer, I will have implemented the Staff Admin Backend for registering a patient

---
+++ ums/src/main/java/gov/samhsa/c2s/ums/service/util/UserDtoToUserMap.java
@@ -0,0 +1,103 @@
+package gov.samhsa.c2s.ums.service.util;
+
+import gov.samhsa.c2s.ums.domain.Locale;
+import gov.samhsa.c2s.ums.domain.LocaleRepository;
+import gov.samhsa.c2s.ums.domain.Role;
+import gov.samhsa.c2s.ums.domain.RoleRepository;
+import gov.samhsa.c2s.ums.domain.User;
+import gov.samhsa.c2s.ums.domain.reference.AdministrativeGenderCode;
+import gov.samhsa.c2s.ums.domain.reference.AdministrativeGenderCodeRepository;
+import gov.samhsa.c2s.ums.service.dto.UserDto;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class UserDtoToUserMap extends PropertyMap<UserDto, User> {
+
+    private final AdministrativeGenderConverter genderConverter;
+    private final RoleConverter roleConverter;
+    private final LocaleConverter localeConverter;
+
+    public UserDtoToUserMap(AdministrativeGenderConverter genderConverter, RoleConverter roleConverter, LocaleConverter localeConverter) {
+        this.genderConverter = genderConverter;
+        this.roleConverter = roleConverter;
+        this.localeConverter = localeConverter;
+    }
+
+
+    @Override
+    protected void configure() {
+        //Required Fields
+        map().setFirstName(source.getFirstName());
+        map().setLastName(source.getLastName());
+        map().setBirthDay(source.getBirthDate());
+        map().setSocialSecurityNumber(source.getSocialSecurityNumber());
+        using(genderConverter).map(source).setAdministrativeGenderCode(null);
+        skip().setAddress(null);
+        using(roleConverter).map(source).setRoles(null);
+        using(localeConverter).map(source).setLocale(null);
+    }
+
+    /**
+     * Converts {@link  UserDto } to {@link AdministrativeGenderCode}}
+     */
+    @Component
+    private static class AdministrativeGenderConverter extends AbstractConverter<UserDto, AdministrativeGenderCode> {
+
+        private final AdministrativeGenderCodeRepository genderCodeRepository;
+
+        @Autowired
+        public AdministrativeGenderConverter(AdministrativeGenderCodeRepository genderCodeRepository) {
+            this.genderCodeRepository = genderCodeRepository;
+        }
+
+        @Override
+        protected AdministrativeGenderCode convert(UserDto source) {
+            return genderCodeRepository.findByCode(source.getGenderCode());
+        }
+    }
+
+
+    /**
+     * Converts {@link  UserDto } to {@link Role}}
+     */
+    @Component
+    private static class RoleConverter extends AbstractConverter<UserDto, Set<Role>> {
+        private final RoleRepository roleRepository;
+
+        @Autowired
+        public RoleConverter(RoleRepository roleRepository) {
+            this.roleRepository = roleRepository;
+        }
+
+        @Override
+        protected Set<Role> convert(UserDto source) {
+            return roleRepository.findAllByCode(source.getRole());
+        }
+    }
+
+
+    /**
+     * Converts {@link  UserDto } to {@link Locale}}
+     */
+    @Component
+    private static class LocaleConverter extends AbstractConverter<UserDto, Locale> {
+        private final LocaleRepository localeRepository;
+
+        @Autowired
+        public LocaleConverter(LocaleRepository localeRepository) {
+            this.localeRepository = localeRepository;
+        }
+
+        @Override
+        protected Locale convert(UserDto source) {
+            return localeRepository.findByCode(source.getLocale());
+        }
+    }
+}
+
+
