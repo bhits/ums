bf1a48856faf66ef7326db5a44089040b0fb205c
CindyLiFei <cindy.li@feisystems.com>
2017-04-20 12:51:10 -0400

    Remove NotNull annotation for address
    #13512: As a developer, I will have integrated UMS with C2S components

--- ums/src/main/java/gov/samhsa/c2s/ums/service/dto/AddressDto.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/service/dto/AddressDto.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -17,31 +16,26 @@ public class AddressDto {
     /**
      * The street address line.
      */
-    @NotNull
     private String streetAddressLine;
 
     /**
      * The city.
      */
-    @NotNull
     private String city;
 
     /**
      * The state code.
      */
-    @NotNull
     private String stateCode;
 
     /**
      * The postal code.
      */
-    @NotNull
     @Pattern(regexp = "\\d{5}(?:[-\\s]\\d{4})?")
     private String postalCode;
 
     /**
      * The country code.
      */
-    @NotNull
     private String countryCode;
 }
