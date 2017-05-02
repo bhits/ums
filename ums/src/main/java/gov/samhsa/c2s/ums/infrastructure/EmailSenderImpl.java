510e820209ca5c500575ea8e67b508dc15cdb281
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-05-02 12:22:05 -0400

    Use user selected locale to provider i18n for activation and verification emails
    #13517 - As a developer, I have integrated i18n support in c2s ui with backend and updated all pages in C2S UI

--- ums/src/main/java/gov/samhsa/c2s/ums/infrastructure/EmailSenderImpl.java
+++ ums/src/main/java/gov/samhsa/c2s/ums/infrastructure/EmailSenderImpl.java
@@ -31,6 +31,18 @@ public class EmailSenderImpl implements EmailSender {
     private static final String TEMPLATE_CONFIRM_VERIFICATION_EMAIL = "confirm-verification-email";
     private static final String PROP_EMAIL_FROM_ADDRESS = "email.from.address";
     private static final String PROP_EMAIL_FROM_PERSONAL = "email.from.personal";
+    private static final String PROP_EMAIL_VERIFICATION_SIGN_OFF = "email.verification.body.signOff";
+    private static final String PROP_EMAIL_VERIFICATION_CREATE_LOGIN_LINK = "email.verification.body.createLoginlink";
+    private static final String PROP_EMAIL_VERIFICATION_MESSAGE = "email.verification.body.message";
+    private static final String PROP_EMAIL_VERIFICATION_GREETING = "email.verification.body.greeting";
+    private static final String PROP_EMAIL_VERIFICATION_BODY_HEADER = "email.verification.body.header";
+
+    private static final String PROP_EMAIL_CONFIRM_VERIFICATION_SIGN_OFF = "email.confirmVerification.body.signOff";
+    private static final String PROP_EMAIL_CONFIRM_VERIFICATION_LOGIN_LINK = "email.confirmVerification.body.loginLink";
+    private static final String PROP_EMAIL_CONFIRM_VERIFICATION_MESSAGE1 = "email.confirmVerification.body.message1";
+    private static final String PROP_EMAIL_CONFIRM_VERIFICATION_MESSAGE2 = "email.confirmVerification.body.message2";
+    private static final String PROP_EMAIL_CONFIRM_VERIFICATION_GREETING = "email.confirmVerification.body.greeting";
+    private static final String PROP_EMAIL_CONFIRM_VERIFICATION_BODY_HEADER = "email.confirmVerification.body.header";
 
     private static final String ENCODING = StandardCharsets.UTF_8.toString();
 
@@ -38,6 +50,19 @@ public class EmailSenderImpl implements EmailSender {
     private static final String PARAM_LINK_URL = "linkUrl";
 
     private static final String PARAM_BRAND = "brand";
+    private static final String PARAM_SIGN_OFF = "signOff";
+    private static final String PARAM_VERIFICATION_CREATE_LOGIN_LINK = "createLoginlink";
+    private static final String PARAM_VERIFICATION_MESSAGE = "message";
+    private static final String PARAM_VERIFICATION_GREETING = "greeting";
+    private static final String PARAM_VERIFICATION_HEADER = "header";
+
+
+    private static final String PARAM_CONFIRM_VERIFICATION_HEADER = "header";
+    private static final String PARAM_CONFIRM_VERIFICATION_GREETING = "greeting";
+    private static final String PARAM_CONFIRM_VERIFICATION_MESSAGE1 = "message1";
+    private static final String PARAM_CONFIRM_VERIFICATION_MESSAGE2 = "message2";
+    private static final String PARAM_CONFIRM_VERIFICATION_LOGIN_LINK = "loginLink";
+    private static final String PARAM_CONFIRM_SIGN_OFF = "signOff";
 
     @Autowired
     private EmailSenderProperties emailSenderProperties;
@@ -52,7 +77,7 @@ public class EmailSenderImpl implements EmailSender {
     private MessageSource messageSource;
 
     @Override
-    public void sendEmailWithVerificationLink(String xForwardedProto, String xForwardedHost, int xForwardedPort, String email, String emailToken, String recipientFullName) {
+    public void sendEmailWithVerificationLink(String xForwardedProto, String xForwardedHost, int xForwardedPort, String email, String emailToken, String recipientFullName, Locale locale) {
         Assert.hasText(emailToken, "emailToken must have text");
         Assert.hasText(email, "email must have text");
         Assert.hasText(recipientFullName, "recipientFullName must have text");
@@ -60,32 +85,50 @@ public class EmailSenderImpl implements EmailSender {
 
         final String verificationUrl = toC2SUIVerificationUri(xForwardedProto, xForwardedHost, xForwardedPort, fragment);
         final Context ctx = new Context();
+        ctx.setLocale(locale);
         ctx.setVariable(PARAM_RECIPIENT_NAME, recipientFullName);
         ctx.setVariable(PARAM_LINK_URL, verificationUrl);
         ctx.setVariable(PARAM_BRAND, emailSenderProperties.getBrand());
+        ctx.setVariable(PARAM_VERIFICATION_HEADER, messageSource.getMessage(PROP_EMAIL_VERIFICATION_BODY_HEADER, null, locale));
+        ctx.setVariable(PARAM_VERIFICATION_GREETING, messageSource.getMessage(PROP_EMAIL_VERIFICATION_GREETING, null, locale));
+        ctx.setVariable(PARAM_VERIFICATION_MESSAGE, messageSource.getMessage(PROP_EMAIL_VERIFICATION_MESSAGE, null, locale));
+        ctx.setVariable(PARAM_VERIFICATION_CREATE_LOGIN_LINK, messageSource.getMessage(PROP_EMAIL_VERIFICATION_CREATE_LOGIN_LINK, null, locale));
+        ctx.setVariable(PARAM_SIGN_OFF, messageSource.getMessage(PROP_EMAIL_VERIFICATION_SIGN_OFF, null, locale));
+
         sendEmail(ctx, email,
                 PROP_EMAIL_VERIFICATION_LINK_SUBJECT,
                 TEMPLATE_VERIFICATION_LINK_EMAIL,
                 PROP_EMAIL_FROM_ADDRESS,
                 PROP_EMAIL_FROM_PERSONAL,
-                Locale.getDefault());
+                locale);
     }
 
     @Override
-    public void sendEmailToConfirmVerification(String xForwardedProto, String xForwardedHost, int xForwardedPort, String email, String recipientFullName) {
+    public void sendEmailToConfirmVerification(String xForwardedProto, String xForwardedHost, int xForwardedPort, String email, String recipientFullName, Locale locale) {
         Assert.hasText(email, "email must have text");
         Assert.hasText(recipientFullName, "recipientFullName must have text");
 
         final Context ctx = new Context();
+        ctx.setLocale(locale);
         ctx.setVariable(PARAM_RECIPIENT_NAME, recipientFullName);
         ctx.setVariable(PARAM_LINK_URL, toC2SUIBaseUri(xForwardedProto, xForwardedHost, xForwardedPort));
+
+        ctx.setVariable(PARAM_CONFIRM_VERIFICATION_HEADER, messageSource.getMessage(PROP_EMAIL_CONFIRM_VERIFICATION_BODY_HEADER, null, locale));
+        ctx.setVariable(PARAM_CONFIRM_VERIFICATION_GREETING, messageSource.getMessage(PROP_EMAIL_CONFIRM_VERIFICATION_GREETING, null, locale));
+        ctx.setVariable(PARAM_CONFIRM_VERIFICATION_MESSAGE1, messageSource.getMessage(PROP_EMAIL_CONFIRM_VERIFICATION_MESSAGE1, null, locale));
+        ctx.setVariable(PARAM_CONFIRM_VERIFICATION_MESSAGE2, messageSource.getMessage(PROP_EMAIL_CONFIRM_VERIFICATION_MESSAGE2, null, locale));
+        ctx.setVariable(PARAM_CONFIRM_VERIFICATION_LOGIN_LINK, messageSource.getMessage(PROP_EMAIL_CONFIRM_VERIFICATION_LOGIN_LINK, null, locale));
+        ctx.setVariable(PARAM_CONFIRM_SIGN_OFF, messageSource.getMessage(PROP_EMAIL_CONFIRM_VERIFICATION_SIGN_OFF, null, locale));
+
+
         ctx.setVariable(PARAM_BRAND, emailSenderProperties.getBrand());
+
         sendEmail(ctx, email,
                 PROP_EMAIL_CONFIRM_VERIFICATION_SUBJECT,
                 TEMPLATE_CONFIRM_VERIFICATION_EMAIL,
                 PROP_EMAIL_FROM_ADDRESS,
                 PROP_EMAIL_FROM_PERSONAL,
-                Locale.getDefault());
+                locale);
     }
 
     private void sendEmail(Context ctx, String email, String subjectPropKey, String templateName, String fromAddressPropKey, String fromPersonalPropKey, Locale locale) {
