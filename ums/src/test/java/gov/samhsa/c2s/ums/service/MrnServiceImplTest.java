d8c1b8a6a06613ab93f7b6ca80779f27563e980c
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2017-06-06 13:31:36 -0400

    Check values in the unit test of the MrnServiceImpl
    #14042 - As a developer, I will have implemented UMS - Unit Testing Cont.

---
+++ ums/src/test/java/gov/samhsa/c2s/ums/service/MrnServiceImplTest.java
@@ -0,0 +1,60 @@
+package gov.samhsa.c2s.ums.service;
+
+import gov.samhsa.c2s.ums.config.UmsProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MrnServiceImplTest {
+
+    @Mock
+    UmsProperties umsProperties;
+
+    @InjectMocks
+    MrnServiceImpl mrnServiceImpl;
+
+    @Test
+    public void testGenerateMrn_whenThereIsaPrefixInMrn(){
+        //Arrange
+        UmsProperties.Mrn mrn=mock(UmsProperties.Mrn.class);
+        String prefix="prefix";
+        int length=6;
+        when(umsProperties.getMrn()).thenReturn(mrn);
+        when(mrn.getPrefix()).thenReturn(prefix);
+        when(mrn.getLength()).thenReturn(length);
+
+        //Act
+        String generatedMrn=mrnServiceImpl.generateMrn();
+
+        //Assert
+        assertEquals(13,generatedMrn.length());
+        assertEquals("PREFIX.",generatedMrn.substring(0,7));
+        assertTrue(generatedMrn.equals(generatedMrn.toUpperCase()));
+    }
+
+    @Test
+    public void testGenerateMrn_whenThereIsNoPrefixInMrn(){
+        //Arrange
+        UmsProperties.Mrn mrn=mock(UmsProperties.Mrn.class);
+        String prefix=null;
+        int length=6;
+        when(umsProperties.getMrn()).thenReturn(mrn);
+        when(mrn.getPrefix()).thenReturn(prefix);
+        when(mrn.getLength()).thenReturn(length);
+
+        //Act
+        String generatedMrn=mrnServiceImpl.generateMrn();
+
+        //Assert
+        assertEquals(6,generatedMrn.length());
+        assertTrue(generatedMrn.equals(generatedMrn.toUpperCase()));
+    }
+}
