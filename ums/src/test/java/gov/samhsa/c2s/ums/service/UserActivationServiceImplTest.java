c804f1ca299555ab9af46a6ac66815d2a15c3cb4
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2017-06-12 13:34:16 -0400

    Add unit test to checkUsername and AssignScopeToUser methods of useractivationServiceImpl
    #14042 - As a developer, I will have implemented UMS - Unit Testing Cont.

--- ums/src/test/java/gov/samhsa/c2s/ums/service/UserActivationServiceImplTest.java
+++ ums/src/test/java/gov/samhsa/c2s/ums/service/UserActivationServiceImplTest.java
@@ -20,8 +20,10 @@ import gov.samhsa.c2s.ums.service.dto.ScopeAssignmentRequestDto;
 import gov.samhsa.c2s.ums.service.dto.UserActivationRequestDto;
 import gov.samhsa.c2s.ums.service.dto.UserActivationResponseDto;
 import gov.samhsa.c2s.ums.service.dto.UserVerificationRequestDto;
+import gov.samhsa.c2s.ums.service.dto.UsernameUsedDto;
 import gov.samhsa.c2s.ums.service.dto.VerificationResponseDto;
 import gov.samhsa.c2s.ums.service.exception.PasswordConfirmationFailedException;
+import gov.samhsa.c2s.ums.service.exception.ScopeDoesNotExistInDBException;
 import gov.samhsa.c2s.ums.service.exception.UserActivationNotFoundException;
 import gov.samhsa.c2s.ums.service.exception.UserIsAlreadyVerifiedException;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
@@ -49,18 +51,19 @@ import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static gov.samhsa.c2s.common.unit.matcher.ArgumentMatchers.matching;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.data.domain.ExampleMatcher.matching;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserActivationServiceImplTest {
-
-    Long userId=30L;
-    String xForwardedProto="xForwardedProto";
-    String xForwardedHost="xForwardedHost";
-    int xForwardedPort=344;
+    Long userId = 30L;
+    String xForwardedProto = "xForwardedProto";
+    String xForwardedHost = "xForwardedHost";
+    int xForwardedPort = 344;
 
     @Mock
     ModelMapper modelMapper;
@@ -95,40 +98,35 @@ public class UserActivationServiceImplTest {
     @Mock
     UserScopeAssignmentRepository userScopeAssignmentRepository;
 
-
     @InjectMocks
-    UserActivationServiceImpl userActivationService=new UserActivationServiceImpl();
+    UserActivationServiceImpl userActivationService = new UserActivationServiceImpl();
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void testInitiateUserActivation(){
+    public void testInitiateUserActivation() {
         //Arrange
-        String verificationCode="EGHR23";
-        String emailToken="token";
-        User user=mock(User.class);
-        UserActivationResponseDto response=mock(UserActivationResponseDto.class);
-        Instant i=Instant.now().plus(Period.ofDays(7));
-        LocalDate localDate=LocalDate.now();
-        Telecom telecom=mock(Telecom.class);
-
-        Telecom.System email=Telecom.System.EMAIL;
-
-        Telecom telecom1=mock(Telecom.class);
-        Telecom telecom2=mock(Telecom.class);
-
-        Locale locale=mock(Locale.class);
-
-        List<Telecom> telecoms=new ArrayList<>();
+        String verificationCode = "EGHR23";
+        String emailToken = "token";
+        User user = mock(User.class);
+        UserActivationResponseDto response = mock(UserActivationResponseDto.class);
+        Instant i = Instant.now().plus(Period.ofDays(7));
+        LocalDate localDate = LocalDate.now();
+        Telecom telecom = mock(Telecom.class);
+        Telecom.System email = Telecom.System.EMAIL;
+        Telecom telecom1 = mock(Telecom.class);
+        Telecom telecom2 = mock(Telecom.class);
+        Locale locale = mock(Locale.class);
+        List<Telecom> telecoms = new ArrayList<>();
         telecoms.add(telecom);
         telecoms.add(telecom1);
         telecoms.add(telecom2);
-        Demographics demographics=mock(Demographics.class);
+        Demographics demographics = mock(Demographics.class);
 
         when(userRepository.getOne(userId)).thenReturn(user);
 
-        UserActivation userActivation=mock(UserActivation.class);
+        UserActivation userActivation = mock(UserActivation.class);
 
         when(emailTokenGenerator.generateEmailToken()).thenReturn(emailToken);
         when(emailSenderProperties.getEmailTokenExpirationInDays()).thenReturn(7);
@@ -137,11 +135,9 @@ public class UserActivationServiceImplTest {
         when(userActivation.isVerified()).thenReturn(false);
 
         when(tokenGenerator.generateToken(7)).thenReturn("3234");
-
         when(userActivationRepository.save(userActivation)).thenReturn(userActivation);
 
-
-        when(modelMapper.map(user,UserActivationResponseDto.class)).thenReturn(response);
+        when(modelMapper.map(user, UserActivationResponseDto.class)).thenReturn(response);
 
         when(user.getDemographics()).thenReturn(demographics);
         when(demographics.getBirthDay()).thenReturn(localDate);
@@ -150,13 +146,10 @@ public class UserActivationServiceImplTest {
         when(userActivation.getEmailTokenExpirationAsInstant()).thenReturn(i);
 
         when(telecom.getSystem()).thenReturn(email);
-
         when(telecom.getValue()).thenReturn("email");
-
-
         when(demographics.getTelecoms()).thenReturn(telecoms);
 
-        AdministrativeGenderCode administrativeGenderCode=mock(AdministrativeGenderCode.class);
+        AdministrativeGenderCode administrativeGenderCode = mock(AdministrativeGenderCode.class);
         when(demographics.getAdministrativeGenderCode()).thenReturn(administrativeGenderCode);
 
         when(administrativeGenderCode.getCode()).thenReturn("code");
@@ -164,38 +157,22 @@ public class UserActivationServiceImplTest {
         when(locale.getCode()).thenReturn("code");
 
         //Act
-
-        UserActivationResponseDto userActivationResponseDto=userActivationService.initiateUserActivation(userId,xForwardedProto,xForwardedHost,xForwardedPort);
+        UserActivationResponseDto userActivationResponseDto = userActivationService.initiateUserActivation(userId, xForwardedProto, xForwardedHost, xForwardedPort);
 
         //Assert
-        assertEquals(response,userActivationResponseDto);
+        assertEquals(response, userActivationResponseDto);
     }
 
-
     @Test
-    public void testInitiateUserActivation_whenUserIsAlreadyVerified_throwsException() throws Exception{
-
+    public void testInitiateUserActivation_Given_UserIsAlreadyVerified_Then_ThrowsException() throws Exception {
         //Arrange
         thrown.expect(UserIsAlreadyVerifiedException.class);
-        String verificationCode="EGHR23";
-        String emailToken="token";
-        User user=mock(User.class);
-        UserActivationResponseDto response=mock(UserActivationResponseDto.class);
-        Instant i=Instant.now().plus(Period.ofDays(7));
-        LocalDate localDate=LocalDate.now();
-        Telecom telecom=mock(Telecom.class);
-
-        Telecom telecom1=mock(Telecom.class);
-        Telecom telecom2=mock(Telecom.class);
-
-        List<Telecom> telecoms=new ArrayList<>();
-        telecoms.add(telecom);
-        telecoms.add(telecom1);
-        telecoms.add(telecom2);
+        String emailToken = "token";
+        User user = mock(User.class);
 
         when(userRepository.getOne(userId)).thenReturn(user);
 
-        UserActivation userActivation=mock(UserActivation.class);
+        UserActivation userActivation = mock(UserActivation.class);
 
         when(emailTokenGenerator.generateEmailToken()).thenReturn(emailToken);
         when(emailSenderProperties.getEmailTokenExpirationInDays()).thenReturn(7);
@@ -204,29 +181,27 @@ public class UserActivationServiceImplTest {
         when(userActivation.isVerified()).thenReturn(true);
 
         //Act
-        UserActivationResponseDto userActivationResponseDto=userActivationService.initiateUserActivation(userId,xForwardedProto,xForwardedHost,xForwardedPort);
+        UserActivationResponseDto userActivationResponseDto = userActivationService.initiateUserActivation(userId, xForwardedProto, xForwardedHost, xForwardedPort);
 
         //Assert
         assertNull(userActivationResponseDto);
     }
 
-
     @Test
-    public void testVerify_whenVerificationcodeAndBirthDate_IsPresent(){
-
+    public void testVerify_Given_VerificationcodeAndBirthDateIsPresent() {
         //Arrange
-        UserVerificationRequestDto userVerificationRequest=mock(UserVerificationRequestDto.class);
-        final String emailToken="token";
-        final String verificationCode="verificationCode";
-        final LocalDate birthDate=LocalDate.MIN;
-        User user=mock(User.class);
-        long userId=30L;
-        String userIdS=String.valueOf(userId);
-        Instant expire=Instant.MAX;
-        final String verificationCodeNullSafe="verificationCode";
-        Demographics demographics=mock(Demographics.class);
+        UserVerificationRequestDto userVerificationRequest = mock(UserVerificationRequestDto.class);
+        final String emailToken = "token";
+        final String verificationCode = "verificationCode";
+        final LocalDate birthDate = LocalDate.MIN;
+        User user = mock(User.class);
+        long userId = 30L;
+        String userIdS = String.valueOf(userId);
+        Instant expire = Instant.MAX;
+        final String verificationCodeNullSafe = "verificationCode";
+        Demographics demographics = mock(Demographics.class);
 
-        UserActivation userActivation=mock(UserActivation.class);
+        UserActivation userActivation = mock(UserActivation.class);
 
         when(userVerificationRequest.getEmailToken()).thenReturn(emailToken);
         when(userVerificationRequest.getVerificationCode()).thenReturn(String.valueOf(verificationCode));
@@ -234,33 +209,30 @@ public class UserActivationServiceImplTest {
 
         when(userActivationRepository.findOneByEmailToken(emailToken)).thenReturn(Optional.ofNullable(userActivation));
         when(userActivation.getUser()).thenReturn(user);
-        when(userActivationRepository.findOneByEmailTokenAndVerificationCode(emailToken,verificationCodeNullSafe)).thenReturn(Optional.ofNullable(userActivation));
+        when(userActivationRepository.findOneByEmailTokenAndVerificationCode(emailToken, verificationCodeNullSafe)).thenReturn(Optional.ofNullable(userActivation));
         when(userActivation.getEmailTokenExpirationAsInstant()).thenReturn(expire);
         when(userActivation.getUser()).thenReturn(user);
         when(user.getId()).thenReturn(userId);
         when(user.getDemographics()).thenReturn(demographics);
         when(user.getDemographics().getBirthDay()).thenReturn(birthDate);
 
-        VerificationResponseDto verificationResponseDto=new VerificationResponseDto(true,userIdS);
-        VerificationResponseDto verificationResponseDto2=userActivationService.verify(userVerificationRequest);
+        //Act
+        VerificationResponseDto verificationResponseDto1 = userActivationService.verify(userVerificationRequest);
 
-        assertEquals(verificationResponseDto,verificationResponseDto2);
+        //Assert
+        assertEquals(new VerificationResponseDto(true, userIdS), verificationResponseDto1);
     }
 
-
     @Test
-    public void testVerify_whenVerificationCodeAndBirthDate_IsNotPresent(){
+    public void testVerify_Given_VerificationCodeAndBirthDateIsNotPresent() {
         //Arrange
-        UserVerificationRequestDto userVerificationRequest=mock(UserVerificationRequestDto.class);
-        final String emailToken="token";
-        final String verificationCode=null;
-        final LocalDate birthDate=null;
-        User user=mock(User.class);
-        long userId=30L;
-        String userIdS=String.valueOf(userId);
-        Instant expire=Instant.MAX;
+        UserVerificationRequestDto userVerificationRequest = mock(UserVerificationRequestDto.class);
+        final String emailToken = "token";
+        final String verificationCode = null;
+        final LocalDate birthDate = null;
+        Instant expire = Instant.MAX;
 
-        UserActivation userActivation=mock(UserActivation.class);
+        UserActivation userActivation = mock(UserActivation.class);
         when(userVerificationRequest.getEmailToken()).thenReturn(emailToken);
         when(userVerificationRequest.getVerificationCode()).thenReturn(verificationCode);
         when(userVerificationRequest.getBirthDate()).thenReturn(birthDate);
@@ -270,35 +242,33 @@ public class UserActivationServiceImplTest {
         when(userActivation.getEmailTokenExpirationAsInstant()).thenReturn(expire);
 
         //Act
-        VerificationResponseDto verificationResponseDto=new VerificationResponseDto(true);
-        VerificationResponseDto verificationResponseDto1=userActivationService.verify(userVerificationRequest);
+        VerificationResponseDto verificationResponseDto = new VerificationResponseDto(true);
+        VerificationResponseDto verificationResponseDto1 = userActivationService.verify(userVerificationRequest);
 
-        //assert
-        assertEquals(verificationResponseDto,verificationResponseDto1);
-
+        //Assert
+        assertEquals(verificationResponseDto, verificationResponseDto1);
     }
 
     @Test
-    public void testFindUserActivationInfoByUserId_whenUserActivationRecordFound(){
-
+    public void testFindUserActivationInfoByUserId_Given_UserActivationRecordFound() {
         //Arrange
-        User user=mock(User.class);
-        UserActivation userActivation=mock(UserActivation.class);
-        LocalDate birthday=LocalDate.MIN;
-        final String verificationCode="verificationCode";
-        Instant tokenExpiration=Instant.MIN;
-        List<Telecom> telecoms=new ArrayList<>();
-        Telecom telecom=mock(Telecom.class);
-        Telecom telecom2=mock(Telecom.class);
+        User user = mock(User.class);
+        UserActivation userActivation = mock(UserActivation.class);
+        LocalDate birthday = LocalDate.MIN;
+        final String verificationCode = "verificationCode";
+        Instant tokenExpiration = Instant.MIN;
+        List<Telecom> telecoms = new ArrayList<>();
+        Telecom telecom = mock(Telecom.class);
+        Telecom telecom2 = mock(Telecom.class);
         telecoms.add(telecom);
         telecoms.add(telecom2);
-        Telecom.System system=Telecom.System.EMAIL;
-        String value="value";
-       Demographics demographics=mock(Demographics.class) ;
+        Telecom.System system = Telecom.System.EMAIL;
+        String value = "value";
+        Demographics demographics = mock(Demographics.class);
 
         when(userRepository.findOne(userId)).thenReturn(user);
         when(userActivationRepository.findOneByUserId(userId)).thenReturn(Optional.ofNullable(userActivation));
-        UserActivationResponseDto response=mock(UserActivationResponseDto.class);
+        UserActivationResponseDto response = mock(UserActivationResponseDto.class);
         when(modelMapper.map(user, UserActivationResponseDto.class)).thenReturn(response);
 
         when(user.getDemographics()).thenReturn(demographics);
@@ -313,19 +283,15 @@ public class UserActivationServiceImplTest {
 
         when(userActivation.isVerified()).thenReturn(true);
 
-
         //Act
-        UserActivationResponseDto userActivationResponseDto=userActivationService.findUserActivationInfoByUserId(userId);
+        UserActivationResponseDto userActivationResponseDto = userActivationService.findUserActivationInfoByUserId(userId);
 
         //Assert
-        assertEquals(response,userActivationResponseDto);
-
+        assertEquals(response, userActivationResponseDto);
     }
 
-
-
     @Test
-    public void testFindUserActivationInfByUserId_whenUserActivationRecord_NotFound() throws Exception{
+    public void testFindUserActivationInfByUserId_Given_UserActivationRecordNotFound_Then_ThrowsUserActivationNotFoundException() throws Exception {
         //Arrange
         thrown.expect(UserActivationNotFoundException.class);
         thrown.expectMessage("No user activation record found for user id: " + userId);
@@ -333,10 +299,47 @@ public class UserActivationServiceImplTest {
         when(userActivationRepository.findOneByUserId(userId)).thenReturn(Optional.empty());
 
         //Act
-        UserActivationResponseDto userActivationResponseDto= userActivationService.findUserActivationInfoByUserId(userId);
+        UserActivationResponseDto userActivationResponseDto = userActivationService.findUserActivationInfoByUserId(userId);
 
         //Assert
         assertNull(userActivationResponseDto);
+    }
+
+    @Test
+    public void testAssignScopeToUser_Given_ScopeExistInDB() {
+        //Arrange
+        ScopeAssignmentRequestDto scopeAssignmentRequestDto = mock(ScopeAssignmentRequestDto.class);
+        String scope = "scope";
+        Scope foundScope = mock(Scope.class);
+        List<String> scopes = new ArrayList<>();
+        scopes.add(scope);
+        UserActivation userActivation = mock(UserActivation.class);
+        List<UserActivation> userActivations = new ArrayList<>();
+        userActivations.add(userActivation);
+        when(scopeAssignmentRequestDto.getScopes()).thenReturn(scopes);
+        when(scopeRepository.findByScopeName(scope)).thenReturn(foundScope);
+
+        when(userActivationRepository.findAll()).thenReturn(userActivations);
+
+        //Act
+        userActivationService.assignScopeToUser(scopeAssignmentRequestDto);
+
+        //Assert
+        verify(scimService).updateUserWithNewGroup(userActivation, foundScope);
+    }
+
+    @Test
+    public void testCheckUsername() {
+        //Arrange
+        UsernameUsedDto usernameUsedDto = mock(UsernameUsedDto.class);
+        String username = "username";
+        when(scimService.checkUsername(username)).thenReturn(usernameUsedDto);
+
+        //Act
+        UsernameUsedDto usernameUsedDto1 = userActivationService.checkUsername(username);
+
+        //Assert
+        assertEquals(usernameUsedDto, usernameUsedDto1);
     }
 
 }
