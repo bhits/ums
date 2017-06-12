1e608980982f4143789eb9171f8b046cb4e2fcd7
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2017-06-12 09:49:16 -0400

    Format the unit test of TokenGeneratorImpl
    #14042 - As a developer, I will have implemented UMS - Unit Testing Cont.

--- ums/src/test/java/gov/samhsa/c2s/ums/service/TokenGeneratorImplTest.java
+++ ums/src/test/java/gov/samhsa/c2s/ums/service/TokenGeneratorImplTest.java
@@ -9,26 +9,26 @@ import static org.junit.Assert.assertTrue;
 
 public class TokenGeneratorImplTest {
 
-    TokenGeneratorImpl tokenGeneratorImpl=new TokenGeneratorImpl();
+    TokenGeneratorImpl tokenGeneratorImpl = new TokenGeneratorImpl();
 
     @Test
-    public void generateToken_withoutPassingMaxLength(){
+    public void generateToken_Given_WithoutPassingMaxLength() {
         //Act
-        String token=tokenGeneratorImpl.generateToken();
+        String token = tokenGeneratorImpl.generateToken();
 
-        //assert
+        //Assert
         assertTrue(StringUtils.hasText(token));
     }
 
     @Test
-    public void generateToken_PassingMaxLength(){
+    public void generateToken_Given_MaxLength_Then_GenerateTokenNotLongerThanMaxLength() {
         //Act
-        int length=7;
-        String token=tokenGeneratorImpl.generateToken(length);
+        int length = 7;
+        String token = tokenGeneratorImpl.generateToken(length);
 
-        //assert
+        //Assert
         assertTrue(StringUtils.hasText(token));
-        assertTrue(token.length()<=length);
+        assertTrue(token.length() <= length);
 
     }
 }
