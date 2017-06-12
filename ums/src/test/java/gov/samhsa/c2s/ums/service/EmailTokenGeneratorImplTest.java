0bd40d0a34970c7d4a807f6d8b3afa95f4e82147
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2017-06-12 09:42:30 -0400

    Format the unit test of emailTokenGenerator
    #14042 - As a developer, I will have implemented UMS - Unit Testing Cont.

--- ums/src/test/java/gov/samhsa/c2s/ums/service/EmailTokenGeneratorImplTest.java
+++ ums/src/test/java/gov/samhsa/c2s/ums/service/EmailTokenGeneratorImplTest.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class EmailTokenGeneratorImplTest {
     @Rule
-    public ExpectedException thrown=ExpectedException.none();
+    public ExpectedException thrown = ExpectedException.none();
 
     @Mock
     private TokenGenerator tokenGenerator;
@@ -35,38 +35,38 @@ public class EmailTokenGeneratorImplTest {
     EmailTokenGeneratorImpl emailTokenGeneratorImpl;
 
     @Test
-    public void testGenerateEmailToken_whenTheUniqueTokenIsGenerated(){
+    public void testGenerateEmailToken_Given_TheUniqueTokenIsGenerated() {
         //Arrange
-        String  token="token";
+        String token = "token";
         when(tokenGenerator.generateToken()).thenReturn(token);
         when(userActivationRepository.findOneByEmailToken(token)).thenReturn(Optional.empty());
 
         //Act
-        String generatedEmailToken=emailTokenGeneratorImpl.generateEmailToken();
+        String generatedEmailToken = emailTokenGeneratorImpl.generateEmailToken();
 
         //Assert
-        assertEquals(token,generatedEmailToken);
+        assertEquals(token, generatedEmailToken);
     }
 
     @Test
-    public void testGenerateEmailToken_whenUniqueTokenIsNotGenerated_ThrowsException() throws Exception{
+    public void testGenerateEmailToken_Given_UniqueTokenIsNotGenerated_Then_ThrowsException() throws Exception {
         //Arrange
         thrown.expect(UniqueValueGeneratorException.class);
-        String token1="token";
-        String token2="token2";
-        UserActivation userActivation1=mock(UserActivation.class);
-        UserActivation userActivation2=mock(UserActivation.class);
+        String token1 = "token";
+        String token2 = "token2";
+        UserActivation userActivation1 = mock(UserActivation.class);
+        UserActivation userActivation2 = mock(UserActivation.class);
         when(tokenGenerator.generateToken()).thenReturn(token1).thenReturn(token1).thenReturn(token2);
         when(userActivationRepository.findOneByEmailToken(token1)).thenReturn(java.util.Optional.of(userActivation1));
         when(userActivationRepository.findOneByEmailToken(token2)).thenReturn(Optional.ofNullable(userActivation2));
 
         //Act
-        String generatedEmailToken=emailTokenGeneratorImpl.generateEmailToken();
+        String generatedEmailToken = emailTokenGeneratorImpl.generateEmailToken();
 
         //Assert
         assertNull(generatedEmailToken);
-        verify(tokenGenerator,times(3)).generateToken();
-        verify(userActivationRepository,times(2)).findOneByEmailToken(token1);
-        verify(userActivationRepository,times(1)).findOneByEmailToken(token2);
+        verify(tokenGenerator, times(3)).generateToken();
+        verify(userActivationRepository, times(2)).findOneByEmailToken(token1);
+        verify(userActivationRepository, times(1)).findOneByEmailToken(token2);
     }
 }
