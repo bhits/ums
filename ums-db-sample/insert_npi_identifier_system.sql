f77a70fccf3c0ff350e2b1c3364615408e7d617e
burcakulug <burcak.ulug@feisystems.com>
2017-06-15 19:16:07 -0400

    Add sample data script to insert NPI Identifier System
    #14044:As a developer I will have implemented backend to support - Provider Managing patient record Workflow
    NOTE: this is a database breaking change

---
+++ ums-db-sample/insert_npi_identifier_system.sql
@@ -0,0 +1,10 @@
+-- ------------------------------------------------------ 
+-- UMS Sample Data 
+-- ------------------------------------------------------ 
+USE ums;
+
+-- 
+-- Insert data for table identifier_system 
+-- 
+insert into identifier_system (display, oid, system, reassignable)
+values	('United States National Provider Identifier', 'urn:oid:2.16.840.1.113883.4.6', 'http://hl7.org/fhir/sid/us-npi', 0);
