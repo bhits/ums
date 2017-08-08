ebefaf8645897c8682d50d306fb2ca68cb592dc4
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-08-08 15:43:20 -0400

    Add insert i18n messages script
    #14568 - As a developer I will provide I18n support to DB lookup values

---
+++ ums-db-sample/insert_i18n_messages.sql
@@ -0,0 +1,43 @@
+-- Pcm Sample Data
+-- ------------------------------------------------------
+
+USE `ums`;
+
+--
+-- Insert i18n messages for Role
+--
+
+insert into i18n_message values (1, 'ROLE.1.NAME','Role name for patient','en', 'Patient');
+insert into i18n_message values (2, 'ROLE.1.NAME','(ES)Role name for patient','es', '(ES)Patient');
+
+insert into i18n_message values (3, 'ROLE.2.NAME','Role name for Parent','en', 'Parent');
+insert into i18n_message values (4, 'ROLE.2.NAME','(ES)Role name for Parent','es', '(ES)Parent');
+
+insert into i18n_message values (5, 'ROLE.3.NAME','Role name for Guardian','en', 'Guardian');
+insert into i18n_message values (6, 'ROLE.3.NAME','(ES)Role name for Guardian','es', '(ES)Guardian');
+
+insert into i18n_message values (7, 'ROLE.4.NAME','Role name for Provider','en', 'Provider');
+insert into i18n_message values (8, 'ROLE.4.NAME','(ES)Role name for Provider','es', '(ES)Provider');
+
+insert into i18n_message values (9, 'ROLE.5.NAME','Role name for System Support','en', 'System Support');
+insert into i18n_message values (10, 'ROLE.5.NAME','(ES)Role name for System Support','es', '(ES)System Support');
+
+insert into i18n_message values (11, 'ROLE.6.NAME','Role name for Staff','en', 'Staff');
+insert into i18n_message values (12, 'ROLE.6.NAME','(ES)Role name for Staff','es', '(ES)Staff');
+
+
+--
+-- Insert i18n messages for Administrative Gender Code
+--
+
+insert into i18n_message values (13, 'ADMINISTRATIVE_GENDER_CODE.1.DISPLAY_NAME','Administrative gender code display name for male','en', 'Male');
+insert into i18n_message values (14, 'ADMINISTRATIVE_GENDER_CODE.1.DISPLAY_NAME','(ES)Administrative gender code display name for male','es', '(ES)Male');
+insert into i18n_message values (15, 'ADMINISTRATIVE_GENDER_CODE.2.DISPLAY_NAME','Administrative gender code display name for female','en', 'Female');
+insert into i18n_message values (16, 'ADMINISTRATIVE_GENDER_CODE.2.DISPLAY_NAME','(ES)Administrative gender code display name for female','es', '(ES)Female');
+
+insert into i18n_message values (17, 'ADMINISTRATIVE_GENDER_CODE.3.DISPLAY_NAME','Administrative gender code display name for Other','en', 'Other');
+insert into i18n_message values (18, 'ADMINISTRATIVE_GENDER_CODE.3.DISPLAY_NAME','(ES)Administrative gender code display name for other','es', '(ES)Other');
+
+insert into i18n_message values (19, 'ADMINISTRATIVE_GENDER_CODE.4.DISPLAY_NAME','Administrative gender code display name for unknown','en', 'Unknown');
+insert into i18n_message values (20, 'ADMINISTRATIVE_GENDER_CODE.4.DISPLAY_NAME','(ES)Administrative gender code display name for unknown','es', '(ES)Unknown');
+
